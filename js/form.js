9d0d4d0c5039c635d1846c016ede28e4de85dcdd
Egor-bog <7548494@gmail.com>
2021-01-18 15:02:36 +0300

    fix:18/01

--- js/form.js
+++ js/form.js
@@ -20,19 +20,24 @@
 jQuery(document).ready(function(){
     jQuery("form").submit(function() { // Событие отправки с формы
         var form_data = jQuery(this).serialize(); // Собираем данные из полей
-        console.log(form_data);
 		jQuery.ajax({
 			type: "POST", // Метод отправки
 			url: "sendform.php", // Путь к PHP обработчику sendform.php
-			data: form_data,
-			/*success: swal({
+            data: form_data,
+			success: swal({
 				title: "Спасибо за заявку!",
                 type: "success",
                 showConfirmButton: false,
-                timer: 2000
-            })*/
+                timer: 200
+            })
         });
         $(this).find('input, textarea').prop('disabled', true);
         event.preventDefault();
+        alert ("Спасибо за заявку!");
+    function restart () {
+        document.getElementById("contactform").reset();
+    };
+    setTimeout(restart, 3000);
     });
+    
 });
