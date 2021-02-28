392c2ead20e15cc9511e90a27d30e9eb9d75c8a6
Egor-bog <7548494@gmail.com>
2021-02-28 15:45:29 +0300

    fix js header

--- js/catalog/constant/root.js
+++ js/catalog/constant/root.js
@@ -1,5 +1,4 @@
-const ROOT_HEADER = document.getElementById('header');
+
 const ROOT_PRODUCTS = document.getElementById('products');
 const ROOT_SHOPPING = document.getElementById('shopping');
-
 const ROOT_baskeet = document.getElementById('nav__basket');
